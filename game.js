aab414b496e3cb8b87938c315594a8251e39776f
Atharva kulkarni <atharvaanantkulkarni@gmail.com>
2020-08-02 17:39:47 +0530

    second comit

---
+++ game.js
@@ -0,0 +1,20 @@
+var gameSettings={
+  maxPowerUps:2,
+  powerUpVel:50,
+}
+var config={
+  width:800,
+  height:600,
+  backgroundColor: 0x000000,
+  scene: [Scene1,Scene2],
+  pixelArt:true,
+  physics:{
+    default:"arcade",
+    arcade:{
+      debug : false,
+      debugShowVelocity:false
+
+    }
+  }
+}
+var game = new Phaser.Game(config);
